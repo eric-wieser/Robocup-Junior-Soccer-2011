1a2654f5b0e6769f678d1f821d1635b85c5ffc4f
Eric Wieser <wieser.eric@gmail.com>
2011-06-07 11:46:32 +0100

    Got the motor working with the motor drivers. Small bug was in the DCMotor code

---
+++ Tests/MotorTest.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using GHIElectronics.NETMF.FEZ;
+using GHIElectronics.NETMF.System;
+using GHIElectronics.NETMF.Hardware;
+using Technobotts.Robotics;
+
+namespace Technobotts.Tests
+{
+	class MotorTest
+	{
+		public static InputPort button = new InputPort((Cpu.Pin)FEZ_Pin.Digital.LDR, true, Port.ResistorMode.PullUp);
+
+		public static void Main()
+		{
+			double i = 0;
+			double step = System.Math.PI / 16;
+			using (DCMotor motor = new DCMotor(PWM.Pin.PWM1, FEZ_Pin.Digital.Di20, FEZ_Pin.Digital.Di21))
+			{
+				while (button.Read())
+				{
+					motor.Speed = MathEx.Sin(i);
+					i += step;
+					Thread.Sleep(50);
+				}
+			}
+		}
+	}
+}
