cef83b2b1a415205b614613e4502d008f0c16e1f
Eric Wieser <wieser.eric@gmail.com>
2011-06-11 10:27:03 +0100

    Added test code for solenoid and button

---
+++ Tests/HelloWorld.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Threading;
+using Microsoft.SPOT;
+
+namespace Technobotts.Tests
+{
+	class HelloWorld
+	{
+		public static void Main()
+		{
+			Debug.Print("Hello World");
+			
+			Thread.Sleep(2000);
+		}
+	}
+}
---
+++ Tests/SolenoidTest.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using GHIElectronics.NETMF;
+using GHIElectronics.NETMF.Hardware;
+
+using GHIElectronics.NETMF.FEZ;
+
+namespace Technobotts.Tests
+{
+	class SolenoidTest
+	{
+		public static int fireTime = 250;
+		public static uint period = 100;
+
+		public static void Main()
+		{
+
+			InputPort button = new InputPort((Cpu.Pin)FEZ_Pin.Digital.LDR, true, Port.ResistorMode.PullUp);
+			OutputPort solenoid = new OutputPort((Cpu.Pin)FEZ_Pin.Digital.Di20, false);
+			OutputPort led = new OutputPort((Cpu.Pin)FEZ_Pin.Digital.LED, false);
+			PWM solenoidPulsed = new PWM((PWM.Pin) FEZ_Pin.PWM.Di10);
+			while (true)
+			{
+				if (!button.Read())
+				{
+					led.Write(true);
+					solenoidPulsed.SetPulse(period, period / 2);
+					Thread.Sleep(fireTime);
+					solenoidPulsed.Set(false);
+					while (!button.Read()) Thread.Sleep(50) ;
+					Thread.Sleep(500);
+					led.Write(false);
+				}
+			}
+
+		}
+	}
+}
