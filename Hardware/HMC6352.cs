fe1dc456a9035dfd368e7cfcc2430c3018e6b744
Eric Wieser <wieser.eric@gmail.com>
2011-06-11 10:17:36 +0100

    Added polling code to sensor

--- Hardware/HMC6352.cs
+++ Hardware/HMC6352.cs
@@ -10,42 +10,46 @@ namespace Technobotts.Hardware
 		public const ushort DefaultAddress = 0x21;
 		public const int ClockSpeed = 100000;
 
-		public enum Command
-		{
-			WriteEEPROM = 'w',
-			ReadEEPROM = 'r',
-			WriteRAM = 'G',
-			ReadRAM = 'g',
-			Sleep = 'S',
-			Wake = 'W',
-			UpdateOffsets = 'O',
-			StartCalibration = 'C',
-			EndCalibration = 'E',
-			SaveMode = 'L',
-			CalculateHeading = 'A'
-		}
+		#region Enum constants
+			public enum Command
+			{
+				WriteEEPROM = 'w',
+				ReadEEPROM = 'r',
+				WriteRAM = 'G',
+				ReadRAM = 'g',
+				Sleep = 'S',
+				Wake = 'W',
+				UpdateOffsets = 'O',
+				StartCalibration = 'C',
+				EndCalibration = 'E',
+				SaveMode = 'L',
+				CalculateHeading = 'A'
+			}
 
-		public enum EEPROMLocation
-		{
-			SlaveAddress = 0x00,
-			MagnetometerXOffsetMSB = 0x01,
-			MagnetometerXOffsetLSB = 0x02,
-			MagnetometerYOffsetMSB = 0x03,
-			MagnetometerYOffsetLSB = 0x04,
-			TimeDelay = 0x05,
-			MeasurementCount = 0x06,
-			SoftwareVersion = 0x07,
-			OperationMode = 0x08
-		}
+			public enum EEPROMLocation
+			{
+				SlaveAddress = 0x00,
+				MagnetometerXOffsetMSB = 0x01,
+				MagnetometerXOffsetLSB = 0x02,
+				MagnetometerYOffsetMSB = 0x03,
+				MagnetometerYOffsetLSB = 0x04,
+				TimeDelay = 0x05,
+				MeasurementCount = 0x06,
+				SoftwareVersion = 0x07,
+				OperationMode = 0x08
+			}
 
-		public enum RAMLocation
-		{
-			OperationMode = 0x74,
-			Output = 0x4E
-		}
+			public enum RAMLocation
+			{
+				OperationMode = 0x74,
+				Output = 0x4E
+			}
+		#endregion
 
 		public class OperationConfiguration
 		{
+			static readonly uint[] MeasurementRates = new uint[] { 1, 5, 10, 20 };
+
 			public enum OperationMode
 			{
 				Standby = 0x00,
@@ -57,7 +61,10 @@ namespace Technobotts.Hardware
 			{
 				Mode = OperationMode.Standby;
 			}
-			static readonly uint[] MeasurementRates = new uint[] { 1, 5, 10, 20 };
+
+			public OperationMode Mode { get; set; }
+			public bool PeriodicOffsetUpdate { get; set; }
+
 			private int _measurementRate = 0;
 			public uint MeasurementRate
 			{
@@ -78,18 +85,23 @@ namespace Technobotts.Hardware
 					return MeasurementRates[_measurementRate];
 				}
 			}
-			public OperationMode Mode { get; set; }
-			public bool PeriodicSetReset { get; set; }
 			public byte toI2CData()
 			{
 				if (Mode == OperationMode.Continuous)
-					return (byte)(_measurementRate << 5 | (PeriodicSetReset ? 1 : 0) << 4 | (byte)Mode);
+					return (byte)(_measurementRate << 5 | (PeriodicOffsetUpdate ? 1 : 0) << 4 | (byte)Mode);
 				else
 					return (byte)Mode;
 			}
 		}
 
-		public OperationConfiguration OperationConfig;
+		private OperationConfiguration _config;
+		public OperationConfiguration OperationConfig {
+			get { return _config; }
+			set {
+				writeRAM(RAMLocation.OperationMode, value.toI2CData());
+				_config = value;
+			}
+		}
 
 		public HMC6352(ushort address = DefaultAddress) :
 			base(new Configuration(address, ClockSpeed))
@@ -124,19 +136,34 @@ namespace Technobotts.Hardware
 		{
 			get
 			{
+				if (OperationConfig.Mode == OperationConfiguration.OperationMode.Standby)
+					CalculateHeading();
+
 				OutputMode = OutputModeType.Heading;
-				return getOutput();
+				int heading = getOutput();
+
+				if (OperationConfig.Mode == OperationConfiguration.OperationMode.Query)
+					CalculateHeading();
+
+				return heading / 10.0;
 			}
 		}
 		public Vector MagnetometerReading
 		{
 			get
 			{
+				if (OperationConfig.Mode == OperationConfiguration.OperationMode.Standby)
+					CalculateHeading();
+
 				double x, y;
 				OutputMode = OutputModeType.MagnetometerX;
 				x = getOutput();
 				OutputMode = OutputModeType.MagnetometerY;
 				y = getOutput();
+
+				if (OperationConfig.Mode == OperationConfiguration.OperationMode.Query)
+					CalculateHeading();
+
 				return new Vector(x, y);
 			}
 		}
@@ -144,11 +171,18 @@ namespace Technobotts.Hardware
 		{
 			get
 			{
+				if (OperationConfig.Mode == OperationConfiguration.OperationMode.Standby)
+					SendCommand(Command.CalculateHeading);
+
 				double x, y;
 				OutputMode = OutputModeType.RawMagnetometerX;
 				x = getOutput();
 				OutputMode = OutputModeType.RawMagnetometerY;
 				y = getOutput();
+
+				if (OperationConfig.Mode == OperationConfiguration.OperationMode.Query)
+					SendCommand(Command.CalculateHeading);
+
 				return new Vector(x, y);
 			}
 		}
@@ -181,92 +215,100 @@ namespace Technobotts.Hardware
 		}
 		#endregion
 
+		#region Command shortcuts
+			public void Sleep() { SendCommand(Command.Sleep); }
+			public void WakeUp() { SendCommand(Command.Wake); }
+			public void StartCalibration() { SendCommand(Command.StartCalibration); }
+			public void EndCalibration() { SendCommand(Command.EndCalibration); }
+			public void SaveMode() { SendCommand(Command.SaveMode); }
+			public void UpdateOffsets() { SendCommand(Command.UpdateOffsets); }
+			private void CalculateHeading() { SendCommand(Command.CalculateHeading); }
+		#endregion
+
 		#region Extra sensor info
-		public ushort Address
-		{
-			set
+			public ushort Address
 			{
-				writeEEPROM(EEPROMLocation.SlaveAddress, (byte) value);
-				this.Config = new I2CDevice.Configuration(value, ClockSpeed);
+				set
+				{
+					writeEEPROM(EEPROMLocation.SlaveAddress, (byte)(value<<1));
+					this.Config = new I2CDevice.Configuration(value, ClockSpeed);
+				}
+				get { return this.Config.Address; }
 			}
-			get { return this.Config.Address; }
-		}
-		public Vector MagnetometerOffsets
-		{
-			get
+			public Vector MagnetometerOffsets
 			{
-				int x = readEEPROM(EEPROMLocation.MagnetometerXOffsetMSB) << 8 |
-					readEEPROM(EEPROMLocation.MagnetometerXOffsetLSB);
-				int y = readEEPROM(EEPROMLocation.MagnetometerXOffsetMSB) << 8 |
-					readEEPROM(EEPROMLocation.MagnetometerXOffsetLSB);
-				return new Vector(x, y);
-			}
-			set
-			{
-				int x = (int)value.X;
-				int y = (int)value.Y;
+				get
+				{
+					int x = readEEPROM(EEPROMLocation.MagnetometerXOffsetMSB) << 8 |
+						readEEPROM(EEPROMLocation.MagnetometerXOffsetLSB);
+					int y = readEEPROM(EEPROMLocation.MagnetometerXOffsetMSB) << 8 |
+						readEEPROM(EEPROMLocation.MagnetometerXOffsetLSB);
+					return new Vector(x, y);
+				}
+				set
+				{
+					int x = (int)value.X;
+					int y = (int)value.Y;
 
-				writeEEPROM(EEPROMLocation.MagnetometerXOffsetMSB, (byte)(x >> 8));
-				writeEEPROM(EEPROMLocation.MagnetometerXOffsetLSB, (byte)x);
-				writeEEPROM(EEPROMLocation.MagnetometerYOffsetMSB, (byte)(y >> 8));
-				writeEEPROM(EEPROMLocation.MagnetometerYOffsetLSB, (byte)y);
+					writeEEPROM(EEPROMLocation.MagnetometerXOffsetMSB, (byte)(x >> 8));
+					writeEEPROM(EEPROMLocation.MagnetometerXOffsetLSB, (byte)x);
+					writeEEPROM(EEPROMLocation.MagnetometerYOffsetMSB, (byte)(y >> 8));
+					writeEEPROM(EEPROMLocation.MagnetometerYOffsetLSB, (byte)y);
+				}
 			}
-		}
-
-		public byte TimeDelay
-		{
-			get { return readEEPROM(EEPROMLocation.TimeDelay); }
-			set { writeEEPROM(EEPROMLocation.TimeDelay, value); }
-		}
-		public byte MeasurementCount
-		{
-			get { return readEEPROM(EEPROMLocation.MeasurementCount); }
-			set { writeEEPROM(EEPROMLocation.MeasurementCount, value); }
-		}
-		public byte SoftwareVersion
-		{
-			get { return readEEPROM(EEPROMLocation.SoftwareVersion); }
-		}
+			public byte TimeDelay
+			{
+				get { return readEEPROM(EEPROMLocation.TimeDelay); }
+				set { writeEEPROM(EEPROMLocation.TimeDelay, value); }
+			}
+			public byte MeasurementCount
+			{
+				get { return readEEPROM(EEPROMLocation.MeasurementCount); }
+				set { writeEEPROM(EEPROMLocation.MeasurementCount, value); }
+			}
+			public byte SoftwareVersion
+			{
+				get { return readEEPROM(EEPROMLocation.SoftwareVersion); }
+			}
 		#endregion
 
 		#region Reading and writing from EEPROM and RAM
+			private I2CTransaction[] readTransaction = new I2CTransaction[] {
+				CreateWriteTransaction(new byte[2]),
+				CreateReadTransaction(new byte[1])
+			};
+			private I2CTransaction[] writeTransaction = new I2CTransaction[] {
+				CreateWriteTransaction(new byte[3])
+			};
 
-		private I2CTransaction[] readTransaction = new I2CTransaction[] {
-			CreateWriteTransaction(new byte[2]),
-			CreateReadTransaction(new byte[1])
-		};
-		private I2CTransaction[] writeTransaction = new I2CTransaction[] {
-			CreateWriteTransaction(new byte[3])
-		};
-
-		protected byte readEEPROM(EEPROMLocation address)
-		{
-			readTransaction[0].Buffer[0] = (byte)Command.ReadEEPROM;
-			readTransaction[0].Buffer[1] = (byte)address;
-			Execute(readTransaction, 500);
-			return readTransaction[1].Buffer[0];
-		}
-		protected void writeEEPROM(EEPROMLocation address, byte value)
-		{
-			writeTransaction[0].Buffer[0] = (byte)Command.WriteEEPROM;
-			writeTransaction[0].Buffer[1] = (byte)address;
-			writeTransaction[0].Buffer[2] = (byte)value;
-			Execute(readTransaction, 500);
-		}
-		protected byte readRAM(RAMLocation address)
-		{
-			readTransaction[0].Buffer[0] = (byte)Command.ReadRAM;
-			readTransaction[0].Buffer[1] = (byte)address;
-			Execute(readTransaction, 500);
-			return readTransaction[1].Buffer[0];
-		}
-		protected void writeRAM(RAMLocation address, byte value)
-		{
-			writeTransaction[0].Buffer[0] = (byte)Command.WriteRAM;
-			writeTransaction[0].Buffer[1] = (byte)address;
-			writeTransaction[0].Buffer[2] = (byte)value;
-			Execute(readTransaction, 500);
-		}
+			protected byte readEEPROM(EEPROMLocation address)
+			{
+				readTransaction[0].Buffer[0] = (byte)Command.ReadEEPROM;
+				readTransaction[0].Buffer[1] = (byte)address;
+				Execute(readTransaction, 500);
+				return readTransaction[1].Buffer[0];
+			}
+			protected void writeEEPROM(EEPROMLocation address, byte value)
+			{
+				writeTransaction[0].Buffer[0] = (byte)Command.WriteEEPROM;
+				writeTransaction[0].Buffer[1] = (byte)address;
+				writeTransaction[0].Buffer[2] = (byte)value;
+				Execute(readTransaction, 500);
+			}
+			protected byte readRAM(RAMLocation address)
+			{
+				readTransaction[0].Buffer[0] = (byte)Command.ReadRAM;
+				readTransaction[0].Buffer[1] = (byte)address;
+				Execute(readTransaction, 500);
+				return readTransaction[1].Buffer[0];
+			}
+			protected void writeRAM(RAMLocation address, byte value)
+			{
+				writeTransaction[0].Buffer[0] = (byte)Command.WriteRAM;
+				writeTransaction[0].Buffer[1] = (byte)address;
+				writeTransaction[0].Buffer[2] = (byte)value;
+				Execute(readTransaction, 500);
+			}
 		#endregion
 	}
 }
