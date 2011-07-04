8a0648228a0736203fd3d1004d3b83e1d5dcfcf8
Andrew Wightwick <pigletiger@gmail.com>
2011-07-04 12:28:04 +0100

    Minor changes to fix motor smoothing etc

--- Robotics/Navigation/ControlledHolonomicDrive.cs
+++ Robotics/Navigation/ControlledHolonomicDrive.cs
@@ -9,7 +9,22 @@ namespace Technobotts.Robotics.Navigation
 	{
 		private PIDController _pid;
 		private IAngleFinder _compass;
-		private readonly double[] pidConstants = new double[3] { 0.5, 1.5, 0.075 };
+		// the next line is for unsmoothed motors
+		// private readonly double[] pidConstants = new double[3] { 0.5, 1.5, 0.075 };
+		// the next line is for smoothed motors (we divide I by 6, and multiply D by 6 because it now 
+		// step only a max of .33, however, we calculate this twice as fast (at 40 Hz) as we calculate the 
+		// target: (20 Hz) 3
+
+		// load of crap above.
+		private const double Ku = 1.185;	// this is the value that it seemed to oscilate at (though the Regulated motor
+											// code is messing with us.
+		private const double Pu = 1.000;	// we think it's about one second for a full oscillation.
+		// ziegler nichols method below for PID:
+		// private readonly double[] pidConstants = new double[3] { 0.6* Ku, 0.6 * 2  * Ku / Pu, 0.6 * Ku * Pu / 8.0 };
+
+		// original calculated values above... private readonly double[] pidConstants = new double[3] { 0.711, 1.422, .088875 };
+		// private readonly double[] pidConstants = new double[3] { 0.711, 1.422, .088875 };
+		private readonly double[] pidConstants = new double[3] { 0.40, 1.10, .088875 };
 
 		public ControlledHolonomicDrive(IAngleFinder compass, params Wheel[] wheels) : base(wheels)
 		{
